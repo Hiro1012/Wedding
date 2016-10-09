81db25c36b3d4024d7c34eb9dd4314cbb5063f95
Hiro1012 <linjh1012@gmail.com>
2016-10-09 13:47:42 +0800

    updateV4

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,7 +20,7 @@
 
     imgLoader([
         'images/thumbnail.jpeg',
-        'images/bg1.png',
+        'images/bg1.jpg',
         'images/letter.png',
         'images/header.png',
         'images/footer.png',
@@ -79,17 +79,17 @@
     });
 
 
-    // window.onload=function(){
-    var mySwiper = new Swiper('.swiper-container', {
-        direction: 'vertical',
-        loop: true,
-        onInit: function (swiper) { //Swiper2.x的初始化是onFirstInit
-            swiperAnimateCache(swiper); //隐藏动画元素
-            swiperAnimate(swiper); //初始化完成开始动画
-        },
-        onSlideChangeEnd: function (swiper) {
-            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-        }
-    });
-    // };
+    window.onload = function () {
+        var mySwiper = new Swiper('.swiper-container', {
+            direction: 'vertical',
+            loop: true,
+            onInit: function (swiper) { //Swiper2.x的初始化是onFirstInit
+                swiperAnimateCache(swiper); //隐藏动画元素
+                swiperAnimate(swiper); //初始化完成开始动画
+            },
+            onSlideChangeEnd: function (swiper) {
+                swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+            }
+        });
+    };
 })();
