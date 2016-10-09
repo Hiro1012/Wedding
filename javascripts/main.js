8c55c28593233c9145efda46200ef043e2009cae
Hiro1012 <linjh1012@gmail.com>
2016-10-09 11:52:36 +0800

    update

--- javascripts/main.js
+++ javascripts/main.js
@@ -65,7 +65,7 @@
 
     imgLoader([
         'images/thumbnail.jpeg',
-        'images/bg1.png',
+        'images/bg1.jpg',
         'images/letter.png',
         'images/header.png',
         'images/footer.png',
